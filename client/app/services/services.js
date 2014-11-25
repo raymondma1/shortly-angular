6e4ee35b9a0a6106543bcd40134d9af803b6ae1f
raymondma1 <raymondxma@gmail.com>
2014-11-24 19:56:15 -0800

    basic req tests passed

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,7 +1,41 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+  var data = {
+    links: []
+  };
+  //link for shortly
+  var link = {
+    url:''
+  };
+
+  var addLink = function() {
+    return $http({
+      method: 'POST',
+      url: '/api/links'
+    })
+    .then(function(resp){
+      return resp.data.links;
+    });
+  };
+
+  var getLinks = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    })
+    .then(function(res){
+      data.links = (res.data);
+      return data.links;
+    });
+  };
+
+  return {
+    data: data,
+    getLinks: getLinks,
+    addLink: addLink,
+    link: link
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
